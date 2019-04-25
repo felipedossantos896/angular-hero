f6fcbf0914f49f280c835b8b1e60279681aaa026
Felipe S. Carneiro <felipedossantos896@gmail.com>
2019-04-25 19:36:15 -0300

    Criando o component MessagesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { Observable. of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -28,5 +28,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-  
+
 }
